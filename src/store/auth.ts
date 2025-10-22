259ed1a392090c866272ce9dd22af27f6a8b0b41
PasanSasmika <pasansasmika198@gmail.com>
2025-10-22 18:15:54 +0530

    Header Update

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -3,7 +3,7 @@ import { persist } from 'zustand/middleware';
 
 interface AuthState {
   token: string | null;
-  user: { id: number; username: string; role: string } | null;
+  user: { id: number; username: string; role: string; fullName:string } | null;
   setAuth: (token: string, user: any) => void;
   logout: () => void;
 }
